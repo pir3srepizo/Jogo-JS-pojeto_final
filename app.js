87ab5b70241a725c2647ef66b3207a3ea69de9ed
Matheus O Pires Repizo <ma_repizo@hotmail.com>
2023-11-30 19:25:49 -0300

    Jogo-JS-projeto_final
    Meu primeiro joguinho feito com JavaScript

---
+++ app.js
@@ -0,0 +1,35 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// alert = alertar no site / console = alertar somente no inspecionar console
+// (while) enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // (if) se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // (else) senão chute você errou
+        // cor roxa comando que funciona, cor azul criando algo novo
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
